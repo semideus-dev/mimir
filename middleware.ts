bc6b37f63f490ad66023339c8961eee197bc944d
Semideus <semideus.dev@gmail.com>
2025-01-10 18:44:54 +0530

    public routes

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api(.*)",
-  "/(.*)",
+  "/",
+  "/docs"
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
